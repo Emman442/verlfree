6e8c381dc341a8a29bd90696cee6047500d60afe
emmanuel ndema <ndemamanuel2002@gmail.com>
2026-03-25 23:03:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-866464414-960ba",
+  "appId": "1:38519442467:web:0ccc9dd4dc713aadf37596",
+  "apiKey": "AIzaSyAQiE3JPEeWk93e8RMtVTctOyqES0bLmFo",
+  "authDomain": "studio-866464414-960ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "38519442467"
+};
